e315348f5f25e5b202c28e83f0f7913ab337553f
James <james@James-PC.(none)>
2011-10-11 21:00:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransMarketApi::Application.config.secret_token = 'dfea44b8589a84b3d5b9a2d698eba9f04c95d367e7992f3791e66a461741552aa7c4e2c7ae0b7f09e91268b5330c9116e3d6b667cb56529d2a2874d698bca353'
